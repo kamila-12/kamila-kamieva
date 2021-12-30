dc5ed1324c9cb68a471f105b29ba0fad9c41af26
kamila-12 <96009211+kamila-12@users.noreply.github.com>
2021-12-30 23:18:19 +0600

    Add files via upload

---
+++ array.py
@@ -0,0 +1,6 @@
+try:
+    list = []
+    while True:
+        list.append(int(input()))
+except:
+        print(list)
---
+++ dictionary.py
@@ -0,0 +1,3 @@
+GPA = {'Aranova_Sabina': 2.9, 'Sarieva_Sezim': 3.9, 'Sheveleva_Karina': 4.0,
+       'Kanatbekova_Madina': 3.8, 'Savarova_Amina': 3.4 }
+print(GPA)
