1d411239cf3fa005cc0df122aae89486c5d7c109
kamila-12 <96009211+kamila-12@users.noreply.github.com>
2022-01-03 19:37:43 +0600

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,15 @@
+def camp (x, y):
+    return x + y
+print(camp(2, 5))
+
+def arg (a, b):
+    return a - b
+print(arg(7, 6))
+
+def ts (x, y):
+    return x * y
+print(ts(10, 9))
+
+def res (x, y):
+    return x / y
+print(res(4, 2))
